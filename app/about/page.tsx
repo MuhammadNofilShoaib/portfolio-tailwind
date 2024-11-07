e66011dd3841602ba1456541e5ced1bcb1b39b30
Muhammad Nofil Shoaib <164288852+MuhammadNofilShoaib@users.noreply.github.com>
2024-11-07 00:41:07 -0800

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,15 +12,15 @@ function page() {
       </div>
     </div>
     <div className="text-customBlue mt-[50px] text-left ml-0 grid grid-rows-3 mdMob:flex mdMob:flex-col mdMob:gap-[40px] customMd:mt-[100px] customMd:ml-[200px] customMd:gap-[60px customLg:ml-[350px] animate-rightAnimate">
-      <div className="ab-1a">
+      <div className="ab-1a scroll">
         <h1 className='font-bold text-2xl customMd:text-3xl'>● 2021 - 2023</h1>
         <p className='text-xl customMd:text-2xl'>I began my academic journey at Cantab Grammar School, where I laid a strong foundation in core subjects. My hard work and dedication were rewarded when I completed matriculation with a 90% score from BSEK. This achievement was a stepping stone that fueled my passion for learning and academic excellence.</p>
       </div>
-      <div className="ab-1b">
+      <div className="ab-1b scroll">
         <h1 className='font-bold text-2xl customMd:text-3xl'>● 2023 - Present</h1>
         <p className='text-xl customMd:text-2xl'>Continuing my education, I attended Govt. Delhi College for my 11th and 12th grades. Here, I further developed my analytical and problem-solving skills, focusing on subjects that would pave the way for my future in technology. The college experience enriched my understanding and prepared me for the next steps in my academic and professional journey.</p>
       </div>
-      <div className="ab-1c pb-4">
+      <div className="ab-1c scroll pb-4">
         <h1 className='font-bold text-2xl customMd:text-3xl'>● 2023 - Present</h1>
         <p className='text-xl customMd:text-2xl'>Eager to enhance my technical skills, I joined the Governor Sindh IT Initiative, where I embarked on a path in web development. Under the guidance of experienced mentors, I honed my skills in front-end technologies, with a strong focus on building dynamic web applications using Next.js, TypeScript, and Tailwind CSS. This program has been transformative, solidifying my expertise and fueling my aspirations as a web developer.</p>
       </div>
